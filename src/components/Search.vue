93d5ce16bcd320e4df659441a28c5f2aeb8e5535
Nematullah Omid <98613991+nomid03@users.noreply.github.com>
2023-01-11 15:09:07 +0100

    search only for test and style is creat

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,36 @@
 <script setup>
-
+import { ref } from "vue";
+let input = ref("");
+const fruits = ["apple", "banana", "orange"];
+function filteredList() {
+  return fruits.filter((fruit) =>
+    fruit.toLowerCase().includes(input.value.toLowerCase())
+  );
+}
 </script>
 
 <template>
     <h1>Search</h1>
+    <input type="text" v-model="input" placeholder="Search fruits..." />
+  <div class="item fruit" v-for="fruit in filteredList()" :key="fruit">
+    <p>{{ fruit }}</p>
+  </div>
+  <div class="item error" v-if="input&&!filteredList().length">
+     <p>No results found!</p>
+  </div>
  
 </template>
 
 <style scoped>
+
+    input[type=text] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: 2px solid red;
+  border-radius: 4px;
+}
+
 
 </style>
