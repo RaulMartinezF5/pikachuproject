d93a09fc56c252048ad181c3dcbfa455757a3dc9
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-01-13 12:21:13 +0100

    filtered for name and type

--- src/stores/pokemonsList.js
+++ src/stores/pokemonsList.js
@@ -15,12 +15,12 @@ export const usePokemonsList = defineStore({
     }),
     actions: {
         async fetchPokemons () {
-            for (let index = 0; index < 51; index++) {                
+            for (let index = 0; index < 101; index++) {                
                 await fetch(`https://pokeapi.co/api/v2/pokemon/${index+1}`)
                 .then(res => res.json())
                 .then(data => {
                     
-                    if(data.name.includes("")){
+                    if(data.name.includes("") && data.types[0].type.name!=""){
                     pokemon.name = data.name.toUpperCase()
                     pokemon.id = data.id
                     pokemon.type = data.types[0].type.name
@@ -29,7 +29,7 @@ export const usePokemonsList = defineStore({
                     if(data.types[1]!=null){
                         pokemon.type2 = data.types[1].type.name
                         pokemon.color2 = "--"+pokemon.type2
-                       
+                    
                     }else{
                         pokemon.type2 = "----"
                         pokemon.color2 = "----"
@@ -37,7 +37,7 @@ export const usePokemonsList = defineStore({
                     
                     pokemon.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index+1}.png`
                     this.pokemonsList.push(pokemon)
-                   
+                
                 }
                 })
             }
